c1edd7fb072a78ae06f71f5e5ac855a2700c1de8
Shanvi soni <153066735+shanvisoni@users.noreply.github.com>
2025-01-15 02:15:38 +0530

    Update useCategory.js

--- client/src/hooks/useCategory.js
+++ client/src/hooks/useCategory.js
@@ -1,6 +1,6 @@
 import { useState,useEffect } from "react";
 import axios from 'axios';
-const API = "http://localhost:5080/api/v1";
+const API = "https://shopsphere-c73p.onrender.com/api/v1";
 
 
 export default function useCategory(){
