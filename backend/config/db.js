c0078de3d04e1054cf1652d40e77b177c4c9d356
Shanvi soni <153066735+shanvisoni@users.noreply.github.com>
2025-01-15 02:06:08 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,3 @@
-// import mongoose from "mongoose";
-// import Colors from "colors";
-
-// const connectDB = async () => {
-//     try {
-//         const conn = await mongoose.connect(process.env.MONGODB_URI);
-//         console.log(`Connected to MongoDB Database at ${conn.connection.host}`.green);
-//     } catch (error) {
-//         console.log(`Error in MongoDB ${error}`.bgRed.white);
-//     }
-// }
-
-// export default connectDB;
-
-
-
-
-
-
-
-
 
 import mongoose from "mongoose";
 import Colors from "colors";
@@ -26,9 +5,8 @@ import Colors from "colors";
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: process.env.DB_NAME,  // Specify the database name here
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            dbName: process.env.DB_NAME  // Specify the database name here
+            
         });
         // console.log(`Connected to MongoDB Database at ${conn.connection.host}`.green);
     } catch (error) {
