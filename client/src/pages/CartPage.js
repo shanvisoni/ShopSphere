b784b61f4cdedaef1a91905ff6c5ab2d2ad57a74
Shanvi soni <153066735+shanvisoni@users.noreply.github.com>
2025-01-15 02:18:49 +0530

    Update CartPage.js

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -3,7 +3,7 @@ import { useCart } from '../context/cart'
 import { useAuth } from '../context/auth'
 import { useNavigate } from 'react-router-dom'
 import Layout from '../component/layout/Layout'
-const API = "http://localhost:5080/api/v1";
+const API = "https://shopsphere-c73p.onrender.com/api/v1";
 
 const CartPage = () => {
     const [cart,setCart]=useCart()
