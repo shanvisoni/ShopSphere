5de12622ebca2e36c87bb28ca4186a8e5d502742
Shanvi soni <153066735+shanvisoni@users.noreply.github.com>
2025-01-15 02:14:00 +0530

    Update Private.js

--- client/src/component/Routes/Private.js
+++ client/src/component/Routes/Private.js
@@ -3,7 +3,7 @@ import {useAuth} from "../../context/auth";
 import { Outlet } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../Spinner";
-const API = "http://localhost:5080/api/v1";
+const API = "https://shopsphere-c73p.onrender.com/api/v1";
 
 export default function PrivateRoute(){
     const [ok,setOk]=useState(false)
