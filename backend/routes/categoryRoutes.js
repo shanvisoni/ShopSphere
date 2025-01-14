75f1e98e4eee1bd840f3cbaafe4ee846972e4403
Shanvi soni <153066735+shanvisoni@users.noreply.github.com>
2025-01-15 01:59:06 +0530

    Update categoryRoutes.js import statements

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { isAdmin, requireSignIn } from '../backend/middlewares/authMiddleware.js'
-import { categoryController, createCategoryController, deleteCategoryController, singleCategoryController, updateCategoryController } from '../backend/controllers/categoryController.js';
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'
+import { categoryController, createCategoryController, deleteCategoryController, singleCategoryController, updateCategoryController } from '../controllers/categoryController.js';
 
 const router = express.Router()
 
